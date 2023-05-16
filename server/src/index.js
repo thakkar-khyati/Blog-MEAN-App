f17fc09fecd3f4c48c785886f6ab7bb283d765ef
thakkar-khyati <thakkar.yati.gmail.com>
2023-05-16 11:02:41 +0530

    after adding whitelisting for cors in the backgrouund and adding crossorigin=anonymous because the images were giving us cors error

--- server/src/index.js
+++ server/src/index.js
@@ -11,8 +11,20 @@ const app = express()
 
 app.use(bodyParser.json())
 
+const whitelist = ['http://localhost:4200', 'http://localhost:4201']
 
-app.use(cors({origin:'http://localhost:4200'}))
+const allowedUrl = {
+    origin: function(origin,callback){
+        if(whitelist.includes(origin)){
+            callback(null,true)
+        }
+        else{
+            callback(new Error('Url not allowed by cors'))
+        }
+    }
+}
+
+app.use(cors(allowedUrl))
 
 app.listen(3000,()=>{
     console.log('server running on 3000')
